cb7017d3e3f0d0c16c55b46293dfbd48d4e7dcd2
Md. Samiul Islam SIddiqui <samiulcsedu22@gmail.com>
2023-12-09 17:25:06 +0600

    Add basic get controller

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StarBound.Models;
+
+namespace StarBound.Controllers
+{
+    [Route("api/StarBound")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<StarShip> GetStarShips()
+        {
+            return new List<StarShip> { 
+                new StarShip {Id=1, Name="Explorer"},
+                new StarShip {Id=2, Name="Rover"}  
+            };
+        }
+    }
+}
---
+++ Models/StarShip.cs
@@ -0,0 +1,8 @@
+﻿namespace StarBound.Models
+{
+    public class StarShip
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
